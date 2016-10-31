82403665bf3e3cde109981c15ee07420a6b9bb79
jasonscharff <jasonscharff@gmail.com>
2016-10-31 11:21:39 -0700

    Add serial read to rasp pi

--- Radio/transmitter_extract/transmitter_extract.ino
+++ Radio/transmitter_extract/transmitter_extract.ino
@@ -1,19 +1,29 @@
 String STARTER = "qNb4";
 String ENDER = "j5st";
 
+int BAUD_RATE = 9600;
+
 void setup()
 {
-  Serial.begin(4800);
+  Serial.begin(BAUD_RATE);
 }
 
 void loop()
 {
-  digitalWrite(3,HIGH);
-  delay(10);
-  String s = "Hello";
-  s = STARTER + s + ENDER;
-  Serial.print(s);
-  delay(10);
-  digitalWrite(3,LOW);
-  delay(10);
+  if (Serial.available()) {
+    String x = Serial.read();
+    if (x != null && x.length() > 0) {
+      digitalWrite(3, HIGH);
+      delay(10);
+      String s = "Hello";
+      s = STARTER + s + ENDER;
+      Serial.print(s);
+      delay(10);
+      digitalWrite(3, LOW);
+      delay(10);
+    }
+
+  }
+
+
 }
