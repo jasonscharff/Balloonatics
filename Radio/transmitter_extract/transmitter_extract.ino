5b5474c9a8b40454ef2934a7ec78f9f5f20f7f6e
jasonscharff <jasonscharff@gmail.com>
2016-10-31 11:50:02 -0700

    Try adding code for software serial

--- Radio/transmitter_extract/transmitter_extract.ino
+++ Radio/transmitter_extract/transmitter_extract.ino
@@ -1,22 +1,30 @@
+#include <SoftwareSerial.h>
+
 String STARTER = "qNb4";
 String ENDER = "j5st";
 
-int BAUD_RATE = 9600;
+int BAUD_RATE_RASP = 9600;
+int TRANSMITTER_SERIAL_BAUD = 4800;
+SoftwareSerial transmitterSerial(0,1); 
 
 void setup()
 {
-  Serial.begin(BAUD_RATE);
+  Serial.begin(BAUD_RATE_RASP);
+  while (!Serial) {
+    //wait for usb serial
+  }
+  transmitterSerial.begin(TRANSMITTER_SERIAL_BAUD);
 }
 
 void loop()
 {
   if (Serial.available()) {
-    String s = Serial.readString();
+    String s = Serial.read();
     if (s.length() > 0) {
       digitalWrite(3, HIGH);
       delay(10);
       s = STARTER + s + ENDER;
-      Serial.print(s);
+      transmitterSerial.print(s);
       delay(10);
       digitalWrite(3, LOW);
       delay(10);
