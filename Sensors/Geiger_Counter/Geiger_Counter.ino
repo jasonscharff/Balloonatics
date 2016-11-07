ff83635627442d470ff79e0b61377e6bd0befcd9
jasonscharff <jasonscharff@gmail.com>
2016-11-07 10:52:52 -0800

    Prepare geiger counter code for transmission

--- Sensors/Geiger_Counter/Geiger_Counter.ino
+++ Sensors/Geiger_Counter/Geiger_Counter.ino
@@ -1,39 +1,48 @@
+#include <ArduinoJson.h>
+
 int geigerCounterPin = A3;
 int geigerCount = 0;
 long currentTime;
 long prevTime;
 int numCounts = 0;
-int calibate = 0.49;
+int calibrationConstant = 0.49;
 bool wasPreviouslyLow;
+int BAUD_RATE = 9600;
 
-void setup() 
-{
-  Serial.begin(9600);
+void setup() {
+  Serial.begin(BAUD_RATE);
   pinMode(geigerCounterPin, INPUT);
   currentTime = millis();
   prevTime = millis();
   wasPreviouslyLow = !analogRead(geigerCounterPin);
 }
 
-void loop() 
-{
-  while(currentTime < 60000)
-  {
+void loop() {
+  while (currentTime < 60000) {
     geigerCount = analogRead(geigerCounterPin);
-    if(geigerCount == HIGH && wasPreviouslyLow == true)
-    {
+    if (geigerCount == HIGH && wasPreviouslyLow == true) {
       wasPreviouslyLow = false;
       numCounts++;
-    } 
-    else if(geigerCount == LOW)
-    {
+    }
+    else if (geigerCount == LOW) {
       wasPreviouslyLow = true;
     }
     currentTime = millis() - prevTime;
   }
-  numCounts = numCounts*calibrate;
-  Serial.println(numCounts);
+  numCounts = numCounts * calibrationConstant;
+  sendNumCounts(numCounts);
+
   numCounts = 0;
   currentTime = 0;
   prevTime = millis();
 }
+
+
+void sendNumCounts(int numCounts) {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["geiger_cpm"] = numCounts;
+  root.printTo(Serial);
+}
+
+
