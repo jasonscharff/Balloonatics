0d661091f52965555329d9aeb546057d3862f2ed
jasonscharff <jasonscharff@gmail.com>
2016-11-10 14:02:27 -0800

    Add untested pressure arduino code.

--- Sensors/Pressure_Sensor_Arduino_Code/Pressure_Sensor_Arduino_Code.ino
+++ Sensors/Pressure_Sensor_Arduino_Code/Pressure_Sensor_Arduino_Code.ino
@@ -1,33 +1,24 @@
-/*
-   Blink based on temperature
-*/
- 
-// Pin 13 has an LED connected on most Arduino boards.
+#include <ArduinoJson.h>
 
-const int led = 13;
 int pressure = A0;
 int pressvalue = 0;
-int time =0;
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT); 
-  Serial.begin(9600);
+const int BAUD_RATE = 9600;
+
+void setup() {
+  Serial.begin(BAUD_RATE);
   analogReference(EXTERNAL);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
- digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);               // wait for a half second
-  pressvalue = analogRead(pressure);
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);               // wait for a half second
-  time = time + 1;
-  Serial.print("time = ");
-  Serial.print(time);
-  Serial.print("    Pressure = ");
-  Serial.println(pressvalue);
- 
+  int rawPressureValue = analogRead(pressure);
+  sendRawPressure(rawPressureValue);
 }
+
+void sendRawPressure(int rawPressure) {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["raw_exterior_pressure"] = rawPressure;
+  root.printTo(Serial);
+}
+
