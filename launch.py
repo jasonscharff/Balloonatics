c712662d3e275c165c9278aaa280c73d5d1ab084
jasonscharff <jasonscharff@gmail.com>
2016-11-11 13:38:20 -0800

    Prepare for gps test.

--- launch.py
+++ launch.py
@@ -20,9 +20,9 @@ from temperature import *
 #arduino links
 BAUD_RATE = 9600
 genericArduinoSerial = serial.Serial('/dev/ttyACM0', BAUD_RATE)
-#pressureSerial = serial.Serial('/dev/ttyACM1', BAUD_RATE)
-#radioSerial = serial.Serial('/dev/ttyACM2', BAUD_RATE)
-#gpsSerial = serial.Serial('/dev/ttyACM3', BAUD_RATE)
+gpsSerial = serial.Serial('/dev/ttyACM1', BAUD_RATE)
+#pressureSerial = serial.Serial('/dev/ttyACM2', BAUD_RATE)
+#radioSerial = serial.Serial('/dev/ttyACM3', BAUD_RATE)
 
 
 
@@ -181,7 +181,7 @@ def main():
     handleRaspberryPiGPIO()
    # handleGenericArduinoSensor()
    # thread.start_new_thread(handleRaspberryPiGPIO, ())
-#   thread.start_new_thread(handleGPSData, ())
+    thread.start_new_thread(handleGPSData, ())
 #   threading.Timer(60, sendToRadio).start()
 
 
