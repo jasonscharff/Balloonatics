5195c20825b749df9b090ee4c4b9ccdc062ec158
jasonscharff <jasonscharff@gmail.com>
2016-11-11 12:31:51 -0800

    Send string + cast.

--- Sensors/Geiger_Counter/Geiger_Counter.ino
+++ Sensors/Geiger_Counter/Geiger_Counter.ino
@@ -41,7 +41,7 @@ void loop() {
 void sendNumCounts(int numCounts) {
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
-  root["geiger_cpm"] = (int)numCounts;
+  root["geiger_cpm"] = String(numCounts);
   char buffer[256];
   root.printTo(buffer, sizeof(buffer));
   Serial.println(buffer);
--- launch.py
+++ launch.py
@@ -52,7 +52,7 @@ def operateCamera():
 
 def handleSerialInput(serial, responseFunction):
     while True:
-        serialInput = serial.readline()
+        serialInput = str(serial.readline())
         if serialInput is not None and len(serialInput) > 0:
             responseFunction(serialInput)
 
