5177d1daa6b31677d46f108a75b3e94e66bb7eb6
jasonscharff <jasonscharff@gmail.com>
2016-11-12 11:40:07 -0800

    Prepare for Niky to cleanup.

--- pressure_rangefinder_cutoff/pressure_rangefinder_cutoff.ino
+++ pressure_rangefinder_cutoff/pressure_rangefinder_cutoff.ino
@@ -10,6 +10,8 @@ const int pwPin = 7;
 double pulse, inches, cm;
 long time;
 
+double INCHES_TO_CM_CONVERSION_FACTOR = 2.54;
+
 //pressure sensor
 #define BME_SCK 13
 #define BME_MISO 12
@@ -18,6 +20,9 @@ long time;
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
+
+
+
 //Adafruit_BME280 bme; // I2C
 Adafruit_BME280 bme(BME_CS); // hardware SPI
 //Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
@@ -25,35 +30,32 @@ Adafruit_BME280 bme(BME_CS); // hardware SPI
 //cutoff
 int cutoffPin = A1;
 
-void setup()
-{
-  Serial.begin(BAUD_RATE);
-  Serial.println(F("BME280 test"));
+//aaron individual
+int blue = A2;
+int white = A3;
+int red = A4;
+int blueVoltage = 0;
+int whiteVoltage = 0;
+int redVoltage = 0;
 
-  if (!bme.begin()) {   
-     Serial.println("Could not find a valid BME280 sensor, check wiring!");
-     while (1);
-  }
-  
+
+
+void setup() {
+  Serial.begin(BAUD_RATE);
+  bme.begin();
   pinMode(cutoffPin, OUTPUT);
+  pinMode(pwPin, INPUT);
 }
 
-void loop()
-{
+void loop() {
   //range finder data collection
-  pinMode(pwPin, INPUT);
   pulse = pulseIn(pwPin, HIGH);
   time = pulseIn(pwPin, HIGH);
   inches = pulse/147.0;
-  cm = inches*2.54;
+  cm = inches*INCHES_TO_CM_CONVERSION_FACTOR;
 
   Serial.print(inches);
-  Serial.print("in, ");
   Serial.print(cm);
-  Serial.print("cm, ");
-  Serial.print("Time:");
-  Serial.print(time);
-  Serial.println();
 
   //collect pressure, temperature, humidity sensor values
   Serial.print("T = ");
