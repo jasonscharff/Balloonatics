f6f807e1cda51ccde5bb409e91e1280e5269f64a
jasonscharff <jasonscharff@gmail.com>
2016-11-12 12:35:52 -0800

    Add try catch.

--- launch.py
+++ launch.py
@@ -111,9 +111,11 @@ def sendToRadio():
 
 def handlePressureSensor():
     def pressureFunction(serialInput):
-        print 'pressure input' + serialInput
-        dictionaryRepresentaion = json.loads(serialInput)
-        addValueToCSV(PRESSURE_ARDUINO_FILENAME, PRESSURE_ARDUINO_FILENAME, dictionaryRepresentaion)
+        try:
+            dictionaryRepresentaion = json.loads(serialInput)
+            addValueToCSV(PRESSURE_ARDUINO_FILENAME, PRESSURE_ARDUINO_FILENAME, dictionaryRepresentaion)
+        except:
+            pass
 
     handleSerialInput(pressureSerial, pressureFunction)
         
