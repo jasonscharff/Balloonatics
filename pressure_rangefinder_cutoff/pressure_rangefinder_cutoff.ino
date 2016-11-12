bfc4735f45cf1175d4162a4058c7754bb5d3f475
jasonscharff <jasonscharff@gmail.com>
2016-11-12 12:21:43 -0800

    Fix pressure/cutoff code.

--- pressure_rangefinder_cutoff/pressure_rangefinder_cutoff.ino
+++ pressure_rangefinder_cutoff/pressure_rangefinder_cutoff.ino
@@ -1,16 +1,18 @@
+#include <Adafruit_BME280.h>
+
 #include <ArduinoJson.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 
+
 const int BAUD_RATE = 9600;
 
 //range finder
 const int pwPin = 7;
-double pulse, inches, cm;
+double pulse;
 long time;
-double INCHES_TIME = 147.0;
-double INCHES_TO_CM_CONVERSION_FACTOR = 2.54;
+double CM_TIME = 57.874;
 
 //pressure sensor
 #define BME_SCK 13
@@ -19,8 +21,6 @@ double INCHES_TO_CM_CONVERSION_FACTOR = 2.54;
 #define BME_CS 10
 
 #define SEALEVELPRESSURE_HPA (1013.25)
-
-
 
 
 //Adafruit_BME280 bme; // I2C
@@ -39,6 +39,9 @@ int whiteVoltage = 0;
 int redVoltage = 0;
 
 
+char CUTOFF_SIGNAL = 'c';
+
+
 
 void setup() {
   Serial.begin(BAUD_RATE);
@@ -48,44 +51,45 @@ void setup() {
 }
 
 void loop() {
+
+  detectSignal();
   //range finder data collection
   pulse = pulseIn(pwPin, HIGH);
   time = pulseIn(pwPin, HIGH);
-  inches = pulse/INCHES_TIME;
-  cm = inches*INCHES_TO_CM_CONVERSION_FACTOR;
+  double cm = pulse/CM_TIME;
 
-  Serial.print(inches);
-  Serial.print(cm);
+  //bme280
+  double temperature = bme.readTemperature();
+  double pressure = bme.readPressure();
+  double bme_altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  double humidity = bme.readHumidity();
+  sendFrequentData(temperature,pressure,bme_altitude, humidity,cm);
 
-  //collect pressure, temperature, humidity sensor values
-  Serial.print("T = ");
-  Serial.println(bme.readTemperature());
+   long current_time = millis();
+    //wait one second
+    while (millis() - current_time < 1000) {
+      detectSignal();
+    }
+}
 
-  Serial.print("P = ");
-  Serial.println(bme.readPressure() / 100.0F);
-
-  Serial.print("Approx. Altitude = ");
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(" m");
-
-  Serial.print("Humidity = ");
-  Serial.print(bme.readHumidity());
-  Serial.println(" %");
-
-  Serial.println();
-
-  //send a signal to relay to trigger cutoff
-  String raspberryPiInput = Serial.readString();
-  if (raspberryPiInput.length() > 0) {
-      StaticJsonBuffer<200> jsonBuffer;
-      JsonObject& root = jsonBuffer.parseObject(raspberryPiInput);
-      boolean shouldCut = root["altitudeReached"];
-      if(shouldCut) {
-        cutoff();
-      }
+void detectSignal() {
+   char piInput = Serial.read();
+  if (piInput == CUTOFF_SIGNAL) {
+    cutoff();
   }
-  
-  delay(1000);
+}
+
+void sendFrequentData(double temperature, double pressure, double bme_altitude, double humidity, double cm) {
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& root = jsonBuffer.createObject();
+    root["exterior_temperature"] = temperature;
+    root["exterior_humidity"] = humidity;
+    root["exterior_pressure"] = double_with_n_digits(pressure, 5);
+    root["estimated_altitude"] = bme_altitude;
+    root["cm_distance"] = cm;
+    char buffer[256];
+    root.printTo(buffer, sizeof(buffer));
+    Serial.println(buffer);
 }
 
 void cutoff() {
