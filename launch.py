af12f08894994304381cb0ac22c6eb12260aa142
jasonscharff <jasonscharff@gmail.com>
2016-11-12 12:47:23 -0800

    Add println/.

--- launch.py
+++ launch.py
@@ -112,6 +112,7 @@ def sendToRadio():
 def handlePressureSensor():
     def pressureFunction(serialInput):
         try:
+            print serialInput
             dictionaryRepresentaion = json.loads(serialInput)
             addValueToCSV(PRESSURE_ARDUINO_FILENAME, PRESSURE_ARDUINO_FILENAME, dictionaryRepresentaion)
         except:
