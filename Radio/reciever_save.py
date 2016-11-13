2c16ec4be0d08bc99760d3c090a763963eb21c94
jasonscharff <jasonscharff@gmail.com>
2016-11-13 11:43:38 -0800

    Add code to handle receiver response.

---
+++ Radio/reciever_save.py
@@ -0,0 +1,32 @@
+BASE_DIRECTORY = '/users/jasonscharff/Documents/ASR_Balloonatics/'
+BAUD_RATE = 9600
+import serial
+import uuid
+import time
+
+FILENAME = ''
+
+
+def readSerial(port):
+    ser = serial.Serial(port, BAUD_RATE)
+    while True:
+        serialInput = ser.readline();
+        if serialInput is not None and len(serialInput) > 0:
+            print (serialInput)
+            appendToFile(serialInput)
+
+def appendToFile(string):
+    with open(FILENAME, "a") as file:
+        file.write(string)
+
+def createFile():
+    global FILENAME
+    FILENAME =  BASE_DIRECTORY + 'radio_response' + str(uuid.uuid4()) + '.txt'
+    with open(FILENAME, 'w') as file:
+        file.write('BEGIN AT ' + str(time.time()))
+
+createFile()
+
+
+
+
