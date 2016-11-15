a034c54a326b8eeedc567757afbc39ab2a2d1510
jasonscharff <jasonscharff@gmail.com>
2016-11-15 13:11:24 -0800

    Fix comment indentation.

--- launch.py
+++ launch.py
@@ -263,7 +263,7 @@ def main():
     thread.start_new_thread(handleGPSData, ())
     thread.start_new_thread(handlePressureSensor, ())
     threading.Timer(60, sendToRadio).start()
-#something needs to occupy the main thread it appears from prelminary testong.
+	#something needs to occupy the main thread
     handleRaspberryPiGPIO()
     
    
