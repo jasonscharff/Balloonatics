cc21c23dafd5a71034a21b0a8f9b78acf8d54877
jasonscharff <jasonscharff@gmail.com>
2016-12-01 12:41:01 -0800

    Remove time delay.

--- launch.py
+++ launch.py
@@ -195,7 +195,6 @@ def handle_raspberry_pi_gpio():
         #add the interior temperature reading to the csv
         add_value_to_csv(GPIO_FILENAME, GPIO_KEYS, dictionary)
         #wait a second before reading more data
-        time.sleep(1)
 
 #function to read data from pressure arduino
 def handle_pressure_sensor():
